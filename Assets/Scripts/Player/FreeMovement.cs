d4ea48711b370c4aadd560862f807ef9ab0d3cb4
Daniel Alfredo Rayo Roldán <ray22933@uvg.edu.gt>
2026-05-17 23:16:38 -0600

    feat: add player inputs

--- Assets/Scripts/Player/FreeMovement.cs
+++ Assets/Scripts/Player/FreeMovement.cs
@@ -27,12 +27,36 @@ public class FreeMovement : MonoBehaviour
         animator = GetComponentInChildren<Animator>();
         Input_Manager.Instance.Actions.Player.Move.performed += OnMove;
         Input_Manager.Instance.Actions.Player.Move.canceled += OnMove;
+        
+        Input_Manager.Instance.Actions.Player.Sprint.performed += OnSprint;
+        Input_Manager.Instance.Actions.Player.Sprint.canceled += OnSprint;
+        
+        Input_Manager.Instance.Actions.Player.Pause.performed += OnPause;
+        Input_Manager.Instance.Actions.Player.Pause.canceled += OnPause;
+        
+        Input_Manager.Instance.Actions.Player.Interact.performed += OnInteract;
+        Input_Manager.Instance.Actions.Player.Interact.canceled += OnInteract;
+        
+        Input_Manager.Instance.Actions.Player.Map.performed += OnShowMap;
+        Input_Manager.Instance.Actions.Player.Map.canceled += OnShowMap;
     }
 
     private void OnDisable()
     {
         Input_Manager.Instance.Actions.Player.Move.performed -= OnMove;
         Input_Manager.Instance.Actions.Player.Move.canceled -= OnMove;
+        
+        Input_Manager.Instance.Actions.Player.Sprint.performed -= OnSprint;
+        Input_Manager.Instance.Actions.Player.Sprint.canceled -= OnSprint;
+        
+        Input_Manager.Instance.Actions.Player.Pause.performed -= OnPause;
+        Input_Manager.Instance.Actions.Player.Pause.canceled -= OnPause;
+        
+        Input_Manager.Instance.Actions.Player.Interact.performed -= OnInteract;
+        Input_Manager.Instance.Actions.Player.Interact.canceled -= OnInteract;
+        
+        Input_Manager.Instance.Actions.Player.Map.performed -= OnShowMap;
+        Input_Manager.Instance.Actions.Player.Map.canceled -= OnShowMap;
     }
 
     void Update()
@@ -43,7 +67,6 @@ public class FreeMovement : MonoBehaviour
 
     public void OnMove(InputAction.CallbackContext ctx)
     {
-        Debug.Log("Someone called me!");
         // animator.SetBool("isWalking", true);
 
         if (ctx.canceled)
@@ -58,6 +81,27 @@ public class FreeMovement : MonoBehaviour
 
         // animator.SetFloat("InputX", moveInput.x);
         // animator.SetFloat("InputY", moveInput.y);
+    }
+    
+    public void OnPause(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Player pressed pause");
+    }
+
+    public void OnShowMap(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Player pressed show map");
+    }
+    
+    public void OnSprint (InputAction.CallbackContext ctx)
+    {
+        
+        Debug.Log("Player pressed sprint");
+    }
+    
+    public void OnInteract (InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Player pressed Interact button");
     }
 
     private void OnTriggerEnter2D(Collider2D other)
